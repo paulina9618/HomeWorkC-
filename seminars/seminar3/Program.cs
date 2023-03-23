41e4b3f586b97494fbf53e2ff2367faf328945cf
Paulina <polina.bordian@gmail.com>
2023-03-23 18:02:25 +0200

    seminar3

---
+++ seminars/seminar3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать четные числа от 1 до N
+
+
+
+string EvenNumbers(int N)
+{
+    string output = "";
+    int index = 2;
+    while (index < N)
+    {
+        output = output + index + " ";
+        index = index +2;
+    }
+    return output;
+}
+
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = GetNumber("Введите число");
+string result = EvenNumbers(N);
+System.Console.WriteLine(result);
+File.WriteAllText("file.txt", result);
+
+
+
+
+
+
+
+
